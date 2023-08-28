ce0bc8f70e973812ae0f369a39985e1f3d684fdf
gustavollanos <gustavo.llanos@outlook.com>
2023-08-28 16:47:42 -0500

    28082023

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        //Modificar el Request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
+        //Validación
+        $this->validate($request, [
+            'name' => 'required|max:30',
+            'username' =>'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:30',
+            'password' => 'required|confirmed|min:8'
+        ]);
+
+        
+        User::create([
+            'name' => $request->name,
+            'username' => Str::slug( $request->username ),
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+        //Autenticar un usuario
+/*         auth()->attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ]); */
+
+        //Otra forma de autenticar
+        auth()->attempt($request->only('email', 'password'));
+
+        //Redireccionar
+        return redirect()->route('posts.index');
+    }
+}
